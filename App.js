2edb952222ddff3d7b45bb158501e079a85b170f
Aniket pagedar <81167148+Aniketpagedar@users.noreply.github.com>
2023-06-02 19:24:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import "./App.css"
+import Todoinput from './components/Todoinput'
+import Todolist from './components/Todolist';
+
+
+function App() {
+  const [listTodo,setListTodo]=useState([]);
+  let addList=(inputText)=>{
+    if(inputText!=='') 
+    setListTodo([...listTodo,inputText])
+  }
+
+  const deleteListItem = (key)=>{
+     let newListTodo = [...listTodo];
+     newListTodo.splice(key,1)
+     setListTodo([...newListTodo])
+  }
+ 
+  
+
+return (
+    
+     <div className="main-container">
+    <div className='container'>
+    <div className="center-container">
+      <Todoinput addList={addList} />
+     <h1 className="app-heading">Todos of the day</h1>
+     <hr/>
+
+     {listTodo.map((listItem,i) =>{
+      return(
+        
+        <Todolist key={i}  index={i}   item={listItem}  deleteItem={deleteListItem} />
+      )
+     }
+    
+    
+     )}
+     
+    </div>
+   </div>
+   </div>
+  )
+    }
+
+
+export default App
